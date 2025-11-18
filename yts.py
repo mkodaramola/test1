20bda6423a3aea1a0ad5e62a97c1a23b03baf39a
Daramola Oluwafemi Michael <71108268+mkodaramola@users.noreply.github.com>
2025-11-18 21:27:43 +0100

    added a to k

--- yts.py
+++ yts.py
@@ -4,6 +4,8 @@ import time
 # Number of times to scroll
 scroll_count = 50  
 
+#abcdefghijk
+
 for i in range(scroll_count):
     pyautogui.scroll(-400)  # Negative value scrolls down
     time.sleep(30)           # Wait 2 seconds before next scroll 1234
